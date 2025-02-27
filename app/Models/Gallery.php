7d6172a3982929a1af1ddaf0915c5e15dfa9e94e
rizkiiaripin <rizkixirpl@gmail.com>
2025-02-27 10:59:31 +0700

    memperbaiki log activity di model

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -3,19 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Models\Activity;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 
 class Gallery extends Model
 {
-    
+    use LogsActivity;
     protected $guarded = ['id'];
     protected $table = 'galleries';
 
     public function getActivitylogOptions(): LogOptions {
-        return LogOptions::defaults()->setDescriptionForEvent(fn(string $eventName) => "Gallery {$eventName}");;
+        return LogOptions::defaults()->setDescriptionForEvent(fn(string $eventName) => "Gallery {$eventName}")->logUnguarded();
         
+    }
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $activity->causer_id = Auth::user()->id;
+        $activity->causer_name = Auth::user()->name;
     }
     public function images() {
         return fn() => $url = asset('images/gallery/'.$this->image) ;
--- app/Models/News.php
+++ app/Models/News.php
@@ -3,17 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class News extends Model
 {
+    use LogsActivity;
+
     protected $guarded = ['id'];
 
-    public function getActivitylogOptions(): LogOptions {
-        return LogOptions::defaults()->setDescriptionForEvent(fn(string $eventName) => "Gallery {$eventName}");;
-        
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->setDescriptionForEvent(fn(string $eventName) => "News {$eventName}")->logUnguarded();
     }
-    public function images() {
-        return fn() => $url = asset('images/news/'.$this->image) ;
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $activity->causer_id = Auth::user()->id;
+        $activity->causer_name = Auth::user()->name;
+    }
+    public function images()
+    {
+        return fn() => $url = asset('images/news/' . $this->image);
     }
 }
