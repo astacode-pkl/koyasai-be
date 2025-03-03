6b04902022a7ae46fe9c2d9b440fe3fda707c68d
sahdanhm <sahdanhusna057@gmail.com>
2025-03-03 13:25:16 +0700

    Add debugbar

--- resources/views/loghistory.blade.php
+++ resources/views/loghistory.blade.php
@@ -1,61 +1,71 @@
-@extends('layout.app') @section('content')
+@extends('layout.app')
+@section('content')
     <x-breadcrumb></x-breadcrumb>
     <x-alert></x-alert>
-    <div class="row">
-        <div class="col-md-12 ">
-            <div class="card">
-                <div class="card-header bg-primary">
-                    <h4 class="mb-0 text-white">Log Histories</h4>
-                </div>
-                <div class="card-body  overflow-x-scroll">
-                    <table class="table  table-striped table-bordered text-center " id="datatable">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>User Id</th>
-                                <th>log Action</th>
-                                <th>Description</th>
-                                <th>Time</th>
-                                <th>Old Data</th>
-                                <th>New Data</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody class="">
-                            @foreach ($loghistories as $loghistory)
-                                <tr id="{{ $loop->iteration }}" class="gradeC">
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $loghistory->user_id }}</td>
-                                    <td>{{ $loghistory->event }}</td>
-                                    <td>{{ $loghistory->description }}</td>
-                                    <td>{{ $loghistory->created_at }}</td>
-                                    <td>{{ !empty($loghistory->properties['old']) ? json_encode($loghistory->properties['old']) : '-' }}</td>
-                                    <td>{{ !empty($loghistory->properties['attributes']) ? json_encode($loghistory->properties['attributes']) : '-' }}</td>
-                                    <td>
-                                        <form action="/log-histories/{{ Crypt::encryptString($loghistory->id) }}"
-                                            method="POST" class="d-inline" id="form_delete">
 
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="btn btn-danger" id="btn_delete"
-                                                onclick="deleteItem(event)">
-                                                <i class="ti ti-trash fs-5"></i></button>
-                                        </form>
-                                    </td>
+    <section class="datatables">
+        <div class="card">
+            <div class="card-body">
+                <div class="mb-2">
+                    <div class="table-responsive m-t-40">
+                        <table id="config-table" class="table border display table-bordered no-wrap d-block overflow-x-auto">
+                            <thead>
+                                <!-- start row -->
+                                <tr>
+                                    <th>No</th>
+                                    <th>User</th>
+                                    <th>User Action</th>
+                                    <th>Subject</th>
+                                    <th>Time</th>
+                                    <th>Old Data</th>
+                                    <th>New Data</th>
+                                    <th>Action</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                <!-- end row -->
+                            </thead>
+                            <tbody>
+                                @foreach ($loghistories as $loghistory)
+                                    <tr id="{{ $loop->iteration }}" class="gradeC">
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $loghistory->causer_name }}</td>
+                                        <td>{{ $loghistory->event }}</td>
+                                        <td>{{ $loghistory->subject_type }}</td>
+                                        <td>{{ $loghistory->created_at }}</td>
+                                        <td>{{ !empty($loghistory->properties['old']) ? json_encode($loghistory->properties['old']) : '-' }}
+                                        </td>
+                                        <td>{{ !empty($loghistory->properties['attributes']) ? json_encode($loghistory->properties['attributes']) : '-' }}
+                                        </td>
+                                        <td>
+                                            <form action="/log-histories/{{ Crypt::encryptString($loghistory->id) }}"
+                                                method="POST" class="d-inline" id="form_delete">
+
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-danger" id="btn_delete"
+                                                    onclick="deleteItem(event)">
+                                                    <i class="ti ti-trash fs-5"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+    </section>
     @push('script')
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="{{ asset('js/sweetalert.js') }}"></script>
         <!-- datatable -->
-        <script src="{{ asset('template/back') }}/dist/libs/jquery/dist/jquery.min.js"></script>
-        <script src="{{ asset('js/datatable.js') }}"></script>
-        <script src="{{ asset('template/back') }}/dist/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="{{ asset('template/backend') }}/dist/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="{{ asset('template/backend') }}/dist/js/datatable/datatable-basic.init.js"></script>
+    @endpush
+    @push('style')
+        <!-- --------------------------------------------------- -->
+        <!-- Datatable -->
+        <!-- --------------------------------------------------- -->
+        <link rel="stylesheet"
+            href="{{ asset('template/backend') }}/dist/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css">
     @endpush
 @endsection
